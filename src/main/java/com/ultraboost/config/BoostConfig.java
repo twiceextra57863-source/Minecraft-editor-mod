769a87f12a2cb72f348d2d01c318aa9ef561ed2d
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-18 00:33:39 +0530

    Create BoostConfig.java

---
+++ src/main/java/com/ultraboost/config/BoostConfig.java
@@ -0,0 +1,9 @@
+package com.ultraboost.config;
+
+public class BoostConfig {
+
+    public static boolean ENABLE_CULLING = true;
+    public static boolean ENABLE_ENTITY_SKIP = true;
+    public static int TARGET_FPS = 300;
+
+}
