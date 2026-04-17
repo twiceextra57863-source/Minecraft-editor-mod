2cfd1ef24bd2bafa43f0429fa577d4263b8c863f
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-18 00:40:12 +0530

    Delete src/main directory

--- src/main/java/com/ultraboost/config/BoostConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ultraboost.config;
-
-public class BoostConfig {
-
-    public static boolean ENABLE_CULLING = true;
-    public static boolean ENABLE_ENTITY_SKIP = true;
-    public static int TARGET_FPS = 300;
-
-}
