2082f24515e11a522131a85bae88314b5defaee6
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-18 01:05:11 +0530

    Delete src/main/java/com/ultraboost/mixin/EntityOptimizer.java

--- src/main/java/com/ultraboost/mixin/EntityOptimizer.java
+++
@@ -1,8 +0,0 @@
-package com.ultraboost.render;
-
-public class EntityOptimizer {
-
-    public static boolean shouldRender(double distance) {
-        return distance < 100; // tune value (60–120 best)
-    }
-}
