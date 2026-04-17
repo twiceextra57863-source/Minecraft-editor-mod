1985bf8753def99c603730449f7a8ef989f05522
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-18 00:32:08 +0530

    Create UltraBoostMod.java

---
+++ src/main/java/com/ultraboost/UltraBoostMod.java
@@ -0,0 +1,11 @@
+package com.ultraboost;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UltraBoostMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[UltraBoost] Mod Loaded!");
+    }
+}
